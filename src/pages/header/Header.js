05bad2454efed4acd169259c00699288a361927a
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-03 19:26:00 +0200

    [added to jwt token]

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -3,19 +3,36 @@ import {Navbar, container , Nav, Container} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+
+  const token = localStorage.getItem("token");
   return (
    <>
    
-   <Navbar bg='dark' data-bs-theme="dark">
-    <Container>
-        <Navbar.Brand href='#none'><strong>NotloggedIn</strong></Navbar.Brand>
-        <Nav className='m-auto'>
-            <Nav.Link as={Link} className='nav-link' to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} className='nav-link' to="/register">Signup</Nav.Link>
-             </Nav>
-    </Container>
+  <Navbar bg={token ? "blue":"dark"} variant='dark'>
+<Container>
+  <Navbar.Brand as={Link} to="">
+    {token ? "Logged-In":"Not-LoggedIn"}
+  </Navbar.Brand>
+  <Nav className='ml-auto'>
+    {token ? (
+      <>
+      <Nav.Link as={Link} to='/dashboard' className='nav-link'>
+      Dashboard
 
-   </Navbar>
+      </Nav.Link>
+      <Nav.Link className='nav-link' onClick={handleLogout}>
+        Logout
+      </Nav.Link>
+      </>
+    ) :(
+      <Nav.Link>
+        
+      </Nav.Link>
+    )}
+  </Nav>
+</Container>
+
+  </Navbar>
    </>
   )
 }
