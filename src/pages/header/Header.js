7081808b91b9e16cb756dbc40c1d323bc4d577ff
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-07 09:39:56 +0200

    [added ]

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,54 +1,48 @@
-import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
-
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
-  const handleLogout = () =>{
-    localStorage.removeItem("token");
-navigate("/Login");
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/Login');
+  };
 
-  }
   return (
-   <>
-   
-  <Navbar bg={token ? "blue":"dark"} variant='dark'>
-<Container>
-  <Navbar.Brand as={Link} to="">
-    {token ? "Logged-In":"Not-LoggedIn"}
-  </Navbar.Brand>
-  <Nav className='ml-auto'>
-    {token ? (
-      <>
-      <Nav.Link as={Link} to='/dashboard' className='nav-link'>
-      Dashboard
+    <>
+      <Navbar bg={token ? 'primary' : 'dark'} variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="">
+            {token ? 'Logged-In' : 'Not-LoggedIn'}
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            {token ? (
+              <>
+                <Nav.Link as={Link} to="/dashboard" className="nav-link">
+                  Dashboard
+                </Nav.Link>
+                <Nav.Link className="nav-link" onClick={handleLogout}>
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/Login" className="nav-link">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register" className="nav-link">
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-      </Nav.Link>
-      <Nav.Link className='nav-link' onClick={handleLogout}>
-        Logout
-      </Nav.Link>
-      </>
-    ) :(
-      <>
-        <Nav.Link as={Link} to={"/login"} className='nav-link'>
-        Login
-        
-        </Nav.Link>
-        <Nav.Link as={Link} to={"/register"} className='nav-link'>
-Signup
-        </Nav.Link>
-      </>
-    
-    )}
-  </Nav>
-</Container>
-
-  </Navbar>
-   </>
-  )
-}
-
-export default Header
+export default Header;
