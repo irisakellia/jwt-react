0f9dcaf05793fc28b33a7fae9d2e416545de54f6
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-08 11:25:58 +0200

    [added ]

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+  const token = localStorage.getItem("token");
+  const[users , setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect (()=>{
+    const fetchUsers = async()=>{
+      try {
+        const result = await fetch("http://localhost:5001/api/users",{
+          headers:{
+            Authorization:`Bearer ${token}`
+
+          }
+        });
+const response = await result.json();
+setUsers(response);
+
+
+
+      } catch (error) {
+        console.error(error.message)
+      }
+      if(token)
+  fetchUsers();
+else
+navigate("/login")
+    }
+  },[token,navigate])
+
+
+
+
   return (
-    <div>
-      <h1>Dashboard hellppoi</h1>
-    </div>
+   <Container className='mt-5'>
+    <Row>
+      <Col>
+      <h1 className='text-center'>Dashboard</h1>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+            {users.map((user)=>{
+              <tr key={user._id}>
+                 <td>{user.name}</td>
+                  <td>{user.email}</td>
+              </tr>
+            })}
+           
+        </tbody>
+      </Table>
+      </Col>
+    </Row>
+
+   </Container>
   )
 }
 
